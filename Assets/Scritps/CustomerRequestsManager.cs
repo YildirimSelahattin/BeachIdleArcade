3e3c1cfb54060cc10cda2c259de71c73bd3e8733
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2023-05-12 15:23:10 +0300

    Serve in progress

---
+++ Assets/Scritps/CustomerRequestsManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerRequestsManager : MonoBehaviour
+{
+    public bool requestDrink = false;
+    public bool requestMassage = false;
+    public int requestNumber;
+
+    void Start()
+    {
+        requestNumber = RandomRequest();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && GameDataManager.Instance.TotalMoney > 0)
+        {
+            StartCoroutine(ResponseToRequests());
+        }
+        else
+        {
+            StopCoroutine(ResponseToRequests());
+        }
+    }
+
+    IEnumerator ResponseToRequests()
+    {
+        for (int timeCounter = 0; timeCounter <= 10; timeCounter++)
+        {
+            Serve();
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+
+        yield return new WaitForSecondsRealtime(40f);
+        requestNumber = RandomRequest();
+    }
+
+    public void Serve()
+    {
+        
+    }
+
+    public int RandomRequest()
+    {
+        return Random.Range(0,2);
+    }
+}
