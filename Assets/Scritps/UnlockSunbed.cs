b74eed61aaeaf42d228c720522b5844fac32c164
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2023-05-15 11:05:45 +0300

    .

--- Assets/Scritps/UnlockSunbed.cs
+++ Assets/Scritps/UnlockSunbed.cs
@@ -34,7 +34,7 @@ public class UnlockSunbed : MonoBehaviour
             }
             else
             {
-                GameObject desk = Instantiate(newSunbed, new Vector3(transform.position.x - 1.7f, -8.8f, transform.position.z + 0.8f)
+                GameObject desk = Instantiate(newSunbed, new Vector3(transform.position.x - 1.4f, -8.8f, transform.position.z + 0.8f)
                     , Quaternion.Euler(0f, 0f, 0f));
             }
 
@@ -121,25 +121,26 @@ public class UnlockSunbed : MonoBehaviour
 
             if (itemID < 100)
             {
-                GameObject desk = Instantiate(newSunbed, new Vector3(transform.position.x - 1.7f, -8.8f, transform.position.z + 0.8f)
-                , Quaternion.Euler(-90f, 0f, 0f));
+                
+                GameObject desk = Instantiate(newSunbed, new Vector3(transform.position.x, -4.8f, transform.position.z)
+                    , Quaternion.Euler(-90f, 0f, 0f));
 
-                desk.transform.DOScale(10.2f, 10f).SetEase(Ease.OutElastic);
+                desk.transform.DOScale(10.5f, 10f).SetEase(Ease.OutElastic);
                 desk.transform.DOScale(10f, 10f).SetDelay(1.1f).SetEase(Ease.OutElastic);
             }
             else
             {
-                GameObject desk = Instantiate(newSunbed, new Vector3(transform.position.x, -8.82f, transform.position.z)
-            , Quaternion.Euler(0f, 0f, 0f));
+                GameObject desk = Instantiate(newSunbed, new Vector3(transform.position.x - 1.4f, -8.8f, transform.position.z + 0.8f)
+                    , Quaternion.Euler(0f, 0f, 0f));
 
-                desk.transform.DOScale(1.2f, 1f).SetEase(Ease.OutElastic);
+                desk.transform.DOScale(1.08f, 1f).SetEase(Ease.OutElastic);
                 desk.transform.DOScale(1f, 1f).SetDelay(1.1f).SetEase(Ease.OutElastic);
             }
 
 
             gameObject.SetActive(false);
 
-            //buildNavMesh.BuildNavMesh();
+            buildNavMesh.BuildNavMesh();
         }
         GameDataManager.Instance.SaveData();
     }
