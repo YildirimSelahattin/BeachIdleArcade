0c5cc8ca04ae265671750409847f41868741a5c5
Selahattin Yıldırım <selahattin_yildirim@outlook.com>
2023-06-01 17:55:46 +0300

    Appeaer Anim

---
+++ Assets/Scritps/AppaerAnimator.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class AppaerAnimator : MonoBehaviour
+{
+    public float scale;
+
+    void Start()
+    {
+      gameObject.transform.DOScale(scale, 1f).SetEase(Ease.OutElastic);
+    }
+}
